25862d32db8cb535f8b307827ca6930a9a9329e2
brunogcar <brunogcar@msn.com>
2009-02-27 00:59:44 +0100

    added SD2 rev 900
    Signed-off-by: mangos <mangos@web.freetheworld.cjb.net>

---
+++ src/bindings/ScriptDev2/scripts/zone/barrens/the_barrens.cpp
@@ -0,0 +1,428 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: The_Barrens
+SD%Complete: 90
+SDComment: Quest support: 1719, 2458, 4921, 6981
+SDCategory: Barrens
+EndScriptData */
+
+/* ContentData
+npc_beaten_corpse
+npc_sputtervalve
+npc_taskmaster_fizzule      remove hack when mangos implement feature/detect spell kind to not aggro
+npc_twiggy_flathead
+at_twiggy_flathead
+EndContentData */
+
+#include "precompiled.h"
+
+/*######
+## npc_beaten_corpse
+######*/
+
+bool GossipHello_npc_beaten_corpse(Player *player, Creature *_Creature)
+{
+    if( player->GetQuestStatus(4921) == QUEST_STATUS_INCOMPLETE || player->GetQuestStatus(4921) == QUEST_STATUS_COMPLETE)
+        player->ADD_GOSSIP_ITEM(0,"Examine corpse in detail...",GOSSIP_SENDER_MAIN,GOSSIP_ACTION_INFO_DEF+1);
+
+    player->SEND_GOSSIP_MENU(3557,_Creature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_beaten_corpse(Player *player, Creature *_Creature, uint32 sender, uint32 action )
+{
+    if(action == GOSSIP_ACTION_INFO_DEF +1)
+    {
+        player->SEND_GOSSIP_MENU(3558,_Creature->GetGUID());
+        player->KilledMonster( 10668,_Creature->GetGUID() );
+    }
+    return true;
+}
+
+/*######
+## npc_sputtervalve
+######*/
+
+bool GossipHello_npc_sputtervalve(Player *player, Creature *_Creature)
+{
+    if (_Creature->isQuestGiver())
+        player->PrepareQuestMenu( _Creature->GetGUID() );
+
+    if( player->GetQuestStatus(6981) == QUEST_STATUS_INCOMPLETE)
+        player->ADD_GOSSIP_ITEM(0,"Can you tell me about this shard?",GOSSIP_SENDER_MAIN,GOSSIP_ACTION_INFO_DEF);
+
+    player->SEND_GOSSIP_MENU(_Creature->GetNpcTextId(),_Creature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_sputtervalve(Player *player, Creature *_Creature, uint32 sender, uint32 action )
+{
+    if(action == GOSSIP_ACTION_INFO_DEF)
+    {
+        player->SEND_GOSSIP_MENU(2013,_Creature->GetGUID());
+        player->AreaExploredOrEventHappens(6981);
+    }
+    return true;
+}
+
+/*######
+## npc_taskmaster_fizzule
+######*/
+
+//#define FACTION_HOSTILE_F       430
+#define FACTION_HOSTILE_F       16
+#define FACTION_FRIENDLY_F      35
+
+#define SPELL_FLARE             10113
+#define SPELL_FOLLY             10137
+
+struct MANGOS_DLL_DECL npc_taskmaster_fizzuleAI : public ScriptedAI
+{
+    npc_taskmaster_fizzuleAI(Creature* c) : ScriptedAI(c) { Reset(); }
+
+    bool IsFriend;
+    uint32 Reset_Timer;
+    uint32 FlareCount;
+
+    void Reset()
+    {
+        IsFriend = false;
+        Reset_Timer = 120000;
+        FlareCount = 0;
+        m_creature->setFaction(FACTION_HOSTILE_F);
+        m_creature->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER);
+    }
+
+    //This is a hack. Spellcast will make creature aggro but that is not
+    //supposed to happen (mangos not implemented/not found way to detect this spell kind)
+    void DoUglyHack()
+    {
+        m_creature->RemoveAllAuras();
+        m_creature->DeleteThreatList();
+        m_creature->CombatStop();
+    }
+
+    void SpellHit(Unit *caster, const SpellEntry *spell)
+    {
+        if( spell->Id == SPELL_FLARE || spell->Id == SPELL_FOLLY )
+        {
+            DoUglyHack();
+            ++FlareCount;
+            if( FlareCount >= 2 )
+            {
+                m_creature->setFaction(FACTION_FRIENDLY_F);
+                IsFriend = true;
+            }
+        }
+    }
+
+    void Aggro(Unit* who) { }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if( IsFriend )
+        {
+            if( Reset_Timer < diff )
+            {
+                EnterEvadeMode();
+            } else Reset_Timer -= diff;
+        }
+
+        if (!m_creature->SelectHostilTarget() || !m_creature->getVictim())
+            return;
+
+        DoMeleeAttackIfReady();
+    }
+};
+CreatureAI* GetAI_npc_taskmaster_fizzule(Creature *_Creature)
+{
+    return new npc_taskmaster_fizzuleAI (_Creature);
+}
+
+bool ReciveEmote_npc_taskmaster_fizzule(Player *player, Creature *_Creature, uint32 emote)
+{
+    if( emote == TEXTEMOTE_SALUTE )
+    {
+        if( ((npc_taskmaster_fizzuleAI*)_Creature->AI())->FlareCount >= 2 )
+        {
+            _Creature->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER);
+            _Creature->HandleEmoteCommand(EMOTE_ONESHOT_SALUTE);
+        }
+    }
+    return true;
+}
+
+/*#####
+## npc_twiggy_flathead
+#####*/
+
+#define SAY_BIG_WILL_READY                  -1000123
+#define SAY_TWIGGY_BEGIN                    -1000124
+#define SAY_TWIGGY_FRAY                     -1000125
+#define SAY_TWIGGY_DOWN                     -1000126
+#define SAY_TWIGGY_OVER                     -1000127
+
+#define C_TWIGGY                            6248
+#define C_BIG_WILL                          6238
+#define C_AFFRAY_CHALLENGER                 6240
+#define QUEST_AFFRAY                        1719
+
+float AffrayChallengerLoc[6][4]=
+{
+    {-1683, -4326, 2.79, 0},
+    {-1682, -4329, 2.79, 0},
+    {-1683, -4330, 2.79, 0},
+    {-1680, -4334, 2.79, 1.49},
+    {-1674, -4326, 2.79, 3.49},
+    {-1677, -4334, 2.79, 1.66}
+};
+
+struct MANGOS_DLL_DECL npc_twiggy_flatheadAI : public ScriptedAI
+{
+    npc_twiggy_flatheadAI(Creature *c) : ScriptedAI(c) {Reset();}
+
+    bool EventInProgress;
+
+    uint32 Event_Timer;
+    uint32 Step;
+    uint32 Challenger_Count;
+    uint32 ChallengerDeath_Timer;
+
+    uint64 PlayerGUID;
+    uint64 BigWillGUID;
+    uint64 AffrayChallenger[6];
+
+    void Reset()
+    {
+        EventInProgress = false;
+
+        Event_Timer = 2000;
+        Step = 0;
+        Challenger_Count = 0;
+        ChallengerDeath_Timer = 0;
+
+        PlayerGUID = 0;
+        BigWillGUID = 0;
+
+        for(uint8 i = 0; i < 6; i++)
+            AffrayChallenger[i] = 0;
+    }
+
+    void Aggro(Unit *who) { }
+
+    bool CanStartEvent(Player *player)
+    {
+        if (!EventInProgress)
+        {
+            EventInProgress = true;
+            PlayerGUID = player->GetGUID();
+            DoScriptText(SAY_TWIGGY_BEGIN, m_creature, player);
+            return true;
+        }
+
+        debug_log("SD2: npc_twiggy_flathead event already in progress, need to wait.");
+        return false;
+    }
+
+    void SetChallengers()
+    {
+        for(uint8 i = 0; i < 6; i++)
+        {
+            Creature* pCreature = m_creature->SummonCreature(C_AFFRAY_CHALLENGER, AffrayChallengerLoc[i][0], AffrayChallengerLoc[i][1], AffrayChallengerLoc[i][2], AffrayChallengerLoc[i][3], TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 600000);
+            if (!pCreature)
+            {
+                debug_log("SD2: npc_twiggy_flathead event cannot summon challenger as expected.");
+                continue;
+            }
+
+            pCreature->setFaction(35);
+            pCreature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+            pCreature->HandleEmoteCommand(EMOTE_ONESHOT_ROAR);
+            AffrayChallenger[i] = pCreature->GetGUID();
+        }
+    }
+
+    void SetChallengerReady(Unit *pUnit)
+    {
+        pUnit->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+        pUnit->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+        pUnit->HandleEmoteCommand(EMOTE_ONESHOT_ROAR);
+        pUnit->setFaction(14);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (!EventInProgress)
+            return;
+
+        if (ChallengerDeath_Timer)
+        {
+            if (ChallengerDeath_Timer < diff)
+            {
+                for(uint8 i = 0; i < 6; i++)
+                {
+                    Creature *challenger = (Creature*)Unit::GetUnit(*m_creature,AffrayChallenger[i]);
+                    if (challenger && !challenger->isAlive() && challenger->isDead())
+                    {
+                        DoScriptText(SAY_TWIGGY_DOWN, m_creature);
+                        challenger->RemoveCorpse();
+                        AffrayChallenger[i] = 0;
+                        continue;
+                    }
+                }
+                ChallengerDeath_Timer = 2500;
+            } else ChallengerDeath_Timer -= diff;
+        }
+
+        if (Event_Timer < diff)
+        {
+            Player *player = (Player*)Unit::GetUnit(*m_creature,PlayerGUID);
+            if (!player || player->isDead())
+                Reset();
+
+            switch(Step)
+            {
+                case 0:
+                    SetChallengers();
+                    ChallengerDeath_Timer = 2500;
+                    Event_Timer = 5000;
+                    ++Step;
+                    break;
+                case 1:
+                    DoScriptText(SAY_TWIGGY_FRAY, m_creature);
+                    if (Unit *challenger = Unit::GetUnit(*m_creature,AffrayChallenger[Challenger_Count]))
+                        SetChallengerReady(challenger);
+                    else Reset();
+                    ++Challenger_Count;
+                    Event_Timer = 25000;
+                    if (Challenger_Count == 6)
+                        ++Step;
+                    break;
+                case 2:
+                    if (Unit *temp = m_creature->SummonCreature(C_BIG_WILL,-1713.79,-4342.09,6.05,6.15,TEMPSUMMON_TIMED_OR_DEAD_DESPAWN,300000))
+                    {
+                        BigWillGUID = temp->GetGUID();
+                        temp->setFaction(35);
+                        temp->GetMotionMaster()->MovePoint(0,-1682.31,-4329.68,2.78);
+                    }
+                    Event_Timer = 15000;
+                    ++Step;
+                    break;
+                case 3:
+                    if (Unit *will = Unit::GetUnit(*m_creature,BigWillGUID))
+                    {
+                        will->setFaction(32);
+                        DoScriptText(SAY_BIG_WILL_READY, will, player);
+                    }
+                    Event_Timer = 5000;
+                    ++Step;
+                    break;
+                case 4:
+                    Unit *will = Unit::GetUnit(*m_creature,BigWillGUID);
+                    if (will && will->isDead())
+                    {
+                        DoScriptText(SAY_TWIGGY_OVER, m_creature);
+                        Reset();
+                    } else if (!will)
+                        Reset();
+                    Event_Timer = 5000;
+                    break;
+            }
+        } else Event_Timer -= diff;
+    }
+};
+
+CreatureAI* GetAI_npc_twiggy_flathead(Creature *_Creature)
+{
+    return new npc_twiggy_flatheadAI (_Creature);
+}
+
+Creature* SelectCreatureInGrid(Player *player, uint32 entry, float range)
+{
+    Creature* pCreature = NULL;
+
+    CellPair pair(MaNGOS::ComputeCellPair(player->GetPositionX(), player->GetPositionY()));
+    Cell cell(pair);
+    cell.data.Part.reserved = ALL_DISTRICT;
+    cell.SetNoCreate();
+
+    MaNGOS::NearestCreatureEntryWithLiveStateInObjectRangeCheck creature_check(*player, entry, true, range);
+    MaNGOS::CreatureLastSearcher<MaNGOS::NearestCreatureEntryWithLiveStateInObjectRangeCheck> searcher(player, pCreature, creature_check);
+
+    TypeContainerVisitor<MaNGOS::CreatureLastSearcher<MaNGOS::NearestCreatureEntryWithLiveStateInObjectRangeCheck>, GridTypeMapContainer> creature_searcher(searcher);
+
+    CellLock<GridReadGuard> cell_lock(cell, pair);
+    cell_lock->Visit(cell_lock, creature_searcher,*(player->GetMap()));
+
+    return pCreature;
+}
+
+bool AreaTrigger_at_twiggy_flathead(Player *player, AreaTriggerEntry *at)
+{
+    if (!player->isDead() && player->GetQuestStatus(QUEST_AFFRAY) == QUEST_STATUS_INCOMPLETE)
+    {
+        if (uint16 slot = player->FindQuestSlot(QUEST_AFFRAY))
+        {
+            //we don't want player to start event if failed already.
+            if (player->GetQuestSlotState(slot) == QUEST_STATE_FAIL)
+                return false;
+        }
+
+        Creature* Twiggy = SelectCreatureInGrid(player, C_TWIGGY, 30);
+
+        if (!Twiggy)
+            return false;
+
+        if (((npc_twiggy_flatheadAI*)Twiggy->AI())->CanStartEvent(player))
+            return true;
+    }
+    return false;
+}
+
+void AddSC_the_barrens()
+{
+    Script *newscript;
+
+    newscript = new Script;
+    newscript->Name = "npc_beaten_corpse";
+    newscript->pGossipHello = &GossipHello_npc_beaten_corpse;
+    newscript->pGossipSelect = &GossipSelect_npc_beaten_corpse;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_sputtervalve";
+    newscript->pGossipHello = &GossipHello_npc_sputtervalve;
+    newscript->pGossipSelect = &GossipSelect_npc_sputtervalve;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_taskmaster_fizzule";
+    newscript->GetAI = &GetAI_npc_taskmaster_fizzule;
+    newscript->pReceiveEmote = &ReciveEmote_npc_taskmaster_fizzule;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_twiggy_flathead";
+    newscript->GetAI = &GetAI_npc_twiggy_flathead;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "at_twiggy_flathead";
+    newscript->pAreaTrigger = &AreaTrigger_at_twiggy_flathead;
+    newscript->RegisterSelf();
+}
