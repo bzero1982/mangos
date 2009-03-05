05ddd75b6686f9e5661eea1525c4f2de79b8e0e0
brunogcar <brunogcar@msn.com>
2009-03-05 07:36:12 +0100

    fixed ArenaTeamHandler.cpp that was not fully saved on last ahbot update
    Signed-off-by: mangos <mangos@web.freethenet.cjb.net>

--- src/game/AuctionHouseHandler.cpp
+++ src/game/AuctionHouseHandler.cpp
@@ -684,4 +684,10 @@ void WorldSession::HandleAuctionListPendingSales( WorldPacket & recv_data )
     /*for(uint32 i = 0; i < count; ++i)
     {
         data << "";                                         // string
-        data << "";                            
+        data << "";                                         // string
+        data << uint32(0);
+        data << uint32(0);
+        data << float(0);
+    }*/
+    SendPacket(&data);
+}
