a4914b08013dd5ba9e1b01333c4ae84f12a9ed4a
brunogcar <brunogcar@msn.com>
2009-04-01 03:54:05 +0200

    turned all capitals into sanctuaries
    Signed-off-by: mangos <mangos@web.freethenet.cjb.net>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6037,7 +6037,7 @@ void Player::UpdateZone(uint32 newZone, uint32 newArea)
             pvpInfo.endTimer = time(0);                     // start toggle-off
     }
 
-    if(zone->flags & AREA_FLAG_SANCTUARY)                   // in sanctuary
+    if((zone->flags & AREA_FLAG_SANCTUARY) || (GetAreaId() == 1519) || (GetAreaId() == 1637)  || (GetAreaId() == 1657)  || (GetAreaId() == 1497)  || (GetAreaId() == 1537)  || (GetAreaId() == 3487)  || (GetAreaId() == 3557)  || (GetAreaId() == 1638))                  // in sanctuary
     {
         SetByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_SANCTUARY);
         if(sWorld.IsFFAPvPRealm())
